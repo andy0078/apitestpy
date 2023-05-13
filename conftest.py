c3f7b633fbebe8838b4422e92e8821ab71062135
anning007 <anningsun2008@126.com>
2023-05-13 13:20:37 +0800

    add

--- conftest.py
+++ conftest.py
@@ -7,15 +7,15 @@ from typing import List
 
 import pytest
 
-from  api.base_api import BaseBuyerApi, BaseSellerApi, BaseManagerApi
-from  api.buyer.login import BuyerLogin
-from  api.manager.goods import AuditGoodsApi
-from  api.manager.login import ManagerLogin
-from  api.seller.goods import AddGoodsApi, GetGoodsSkuInfoApi, GoodsUnderApi, DeleteGoodsApi
-from  api.seller.login import SellerLogin
-from  common.db_util import DB_Util
-from  common.file_load import load_yaml_file
-from  common.redis_util import RedisUtil
+from api.base_api import BaseBuyerApi, BaseSellerApi, BaseManagerApi
+from api.buyer.login import BuyerLogin
+from api.manager.goods import AuditGoodsApi
+from api.manager.login import ManagerLogin
+from api.seller.goods import AddGoodsApi, GetGoodsSkuInfoApi, GoodsUnderApi, DeleteGoodsApi
+from api.seller.login import SellerLogin
+from common.db_util import DB_Util
+from common.file_load import load_yaml_file
+from common.redis_util import RedisUtil
 
 
 def pytest_collection_modifyitems(
@@ -26,22 +26,28 @@ def pytest_collection_modifyitems(
         item.name = item.name.encode("utf-8").decode("unicode-escape")
         item._nodeid = item._nodeid.encode("utf-8").decode("unicode-escape")
 
+
 # 获取token
-@pytest.fixture(scope='session',autouse=True)
+@pytest.fixture(scope='session', autouse=True)
 def get_buyer_token():
     buyer_login = BuyerLogin()
     resp = buyer_login.send()
     BaseBuyerApi.buyer_token = resp.json()['access_token']
-@pytest.fixture(scope='session',autouse=True)
+
+
+@pytest.fixture(scope='session', autouse=True)
 def get_seller_token():
     seller_login = SellerLogin()
     resp = seller_login.send()
     BaseSellerApi.seller_token = resp.json()['access_token']
-@pytest.fixture(scope='session',autouse=True)
+
+
+@pytest.fixture(scope='session', autouse=True)
 def get_manager_token():
     manager_login = ManagerLogin()
     resp = manager_login.send()
     BaseManagerApi.manager_token = resp.json()['access_token']
+
 
 # 这个是用来提供已经审核通过的产品信息的
 @pytest.fixture(scope='class')
@@ -56,21 +62,22 @@ def get_goods():
     resp = audit_goods_api.send()
     resp = GetGoodsSkuInfoApi(goods_id).send()
     sku_id = resp.json()[0]['sku_id']
-    yield goods_id,sku_id # 返回多个值时结果是个元组 (7473,7399)
+    yield goods_id, sku_id  # 返回多个值时结果是个元组 (7473,7399)
     # 后置处理，清除该条商品
-    GoodsUnderApi([str(goods_id)]).send()#先下架
+    GoodsUnderApi([str(goods_id)]).send()  # 先下架
     DeleteGoodsApi([str(goods_id)]).send()
 
-@pytest.fixture(scope='session',autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def redis_util():
     redis_info = load_yaml_file('/config/redis.yml')['mtxshop']
-    redis_util = RedisUtil(host=redis_info['host'],pwd=redis_info['password'])
+    redis_util = RedisUtil(host=redis_info['host'], pwd=redis_info['password'])
     yield redis_util
 
-@pytest.fixture(scope='session',autouse=True)
+
+@pytest.fixture(scope='session', autouse=True)
 def db_util():
     db_info = load_yaml_file('/config/db.yml')['mtxshop']
     db_util = DB_Util(host=db_info['host'], user=db_info['username'], password=db_info['password'])
     yield db_util
     db_util.close()
-
